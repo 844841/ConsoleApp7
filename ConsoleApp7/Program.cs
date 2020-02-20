0ea9c446057b0cb63b6972c481350cd64bb3f562
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 14:46:44 +0530

    Initial commit

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[50] ;
+            Console.WriteLine("enter names");
+            for (int i = 0; i < 5; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+            Console.WriteLine("names are");
+                for(int j=0;j<5;j++)
+                {
+                    Console.WriteLine(names[j]);
+                }
+            
+           
+        }
+    }
+}
---
+++ ConsoleApp7/string.cs
@@ -0,0 +1,3 @@
+﻿1>------ Build started: Project: ConsoleApp7, Configuration: Debug Any CPU ------
+1>  ConsoleApp7 -> d:\Users\Admin\source\repos\ConsoleApp7\ConsoleApp7\bin\Debug\ConsoleApp7.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
